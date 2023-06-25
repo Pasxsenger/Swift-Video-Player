9cbd2713cf69949a6b4cbf91011cd214aa6d05a0
Xuesong Shen <pasxsenger@Xuesongs-MBP.attlocal.net>
2023-06-24 19:21:00 -0700

    second commit

---
+++ video_player/video_player/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Xuesong Shen on 6/22/23.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "DoNotSayGoodbye", ofType: "MP4")
+        else {
+            debugPrint("DoNotSayGoodbye.MP4 not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
+
